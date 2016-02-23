e9c2aabbbac0ecd2aafc478e891a88942b1c87f1
degivan <id1997@yandex.ru>
2016-02-23 23:10:33 +0300

    AbstractClient added

--- app/src/main/java/com/example/foodhunter/MainActivity.java
+++ app/src/main/java/com/example/foodhunter/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.foodhunter;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
+
+import spbau.mit.degtjarenko.foodhunter.dishes.Place;
 
 public class MainActivity extends AppCompatActivity {
 
